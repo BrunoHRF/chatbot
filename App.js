c43b1ec0f5eade8adc255207b2d1302786510ea9
BrunoHRF <BrunoHRF@outlook.com>
2020-02-06 20:03:40 -0300

    Api criada

---
+++ App.js
@@ -0,0 +1,28 @@
+var express = require('express'),
+    app = express();
+
+app.get('/', function(req, res){
+    var apiResponse = [];
+    var request = require('request');
+    request({
+        url: ' https://api.github.com/orgs/takenet/repos?language:C#%sort:created',
+        headers: { 'user-agent': 'brunohrf' },
+        json: true
+    }, function (error, response, body) {
+        if(!error && response.statusCode === 200) {
+            apiResponse = body.slice(0,5);
+            let repos = [];
+            for(let i = 0; i < 5; i++) {
+                repos.push({
+                    name: apiResponse[i].full_name,
+                    description: apiResponse[i].description,
+                    avatar: apiResponse[i].owner.avatar_url
+                });
+            }
+            res.send(repos);
+        }
+    });
+});
+
+var server = app.listen(3000);
+console.log('Servidor Express iniciado na porta %s', server.address().port);
